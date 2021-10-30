880ce0b1df3c8bfe04192ea47bfe661390579206
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-10-30 16:26:17 -0400

    Create useCounter.ts

---
+++ hooks/useCounter/useCounter.ts
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const useCounter = (initialValue: number) => {
+  const [value, setValue] = useState(initialValue);
+
+  const increment = () => setValue(c => c + 1);
+  const decrement = () => setValue(c => c - 1);
+  const reset = () => setValue(initialValue);
+
+  return { value, increment, decrement, reset };
+};
+
+export default useCounter;
