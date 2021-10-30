af5e96ec74561fb6096cb383545d5ff31bf75f5e
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-10-30 08:47:52 -0400

    Create useWindowWidth.js

---
+++ hooks/useWindowWidth/useWindowWidth.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+// custom hook
+const useWindowWidth = () => {
+  const [width, setWidth] = React.useState(null);
+
+  // sets width when window is resized (from event listener)
+  const handleWindowResize = () => setWidth(window.innerWidth);
+
+  React.useEffect(() => {
+    // only runs if in browser
+    if (typeof window !== "undefined") {
+      setWidth(window.innerWidth); // sets initial width
+      window.addEventListener("resize", handleWindowResize, true);
+      window.addEventListener("fullscreenchange", handleWindowResize, true);
+
+      return () => {
+        window.removeEventListener("resize", handleWindowResize, true);
+        window.removeEventListener(
+          "fullscreenchange",
+          handleWindowResize,
+          true
+        );
+      };
+    }
+  }, []);
+
+  return width ? width : 0;
+};
+
+export default useWindowWidth;
