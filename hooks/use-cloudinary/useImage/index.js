d45b3a39547a0204b88f9009739dad3633401d49
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-10-31 10:52:46 -0400

    Create index.js

---
+++ hooks/use-cloudinary/useImage/index.js
@@ -0,0 +1,9 @@
+import createCloudinaryUrl from '../../utils/createCloudinaryUrl';
+
+export default function useImage(cloudName) {
+  const generateImageUrl = createCloudinaryUrl(cloudName, "image");
+
+  return {
+    generateImageUrl,
+  };
+}
