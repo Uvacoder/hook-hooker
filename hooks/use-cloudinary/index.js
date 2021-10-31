b320d36cc4f0f263fa0f2d7b5caf776b0ac1cd75
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-10-31 10:57:57 -0400

    Add files via upload

---
+++ hooks/use-cloudinary/index.js
@@ -0,0 +1,11 @@
+import useImage from './hooks/useImage'
+import useVideo from './hooks/useVideo'
+import useGif from './hooks/useGif'
+import useAudio from './hooks/useAudio';
+
+export {
+  useImage,
+  useVideo,
+  useGif,
+  useAudio,
+}
